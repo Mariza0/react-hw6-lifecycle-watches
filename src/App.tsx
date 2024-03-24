3b5117a11877697787c7ce2e1e07e199fb4b4160
Mariza0 <maria.net@mail.ru>
2024-03-24 22:24:41 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { Card } from './components/Сard'
+
+function App() {
+  
+
+  return (
+    
+      <div className="container">
+    
+            <Card  title="Card title"
+                   description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                   link="Go somewhere" 
+            >
+                <img className="card-img" src="src/img/card.png" alt="Card image" />
+            </Card>
+
+            <Card  title="Special title treatment"
+                   description="Whith supporting text below as a natural lead-in to additional content"
+                   link="Go somewhere" 
+            >
+            </Card>
+  
+      </div>
+  )
+}
+
+export default App
