5f9ca2cf1639e4641c8adb95758e411a313ff2ba
Mariza0 <maria.net@mail.ru>
2024-03-25 20:32:20 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "react-hw5-composition-cards",
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: '', // Установите здесь путь к вашим ресурсам, если он отличается от корневого каталога
-  },
 })
