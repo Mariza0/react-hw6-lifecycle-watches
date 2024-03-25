2a3333b10f5880c6816bc21fc348adb673780e97
Mariza0 <maria.net@mail.ru>
2024-03-25 18:50:34 +0100

    change dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    assetsDir: '', // Установите здесь путь к вашим ресурсам, если он отличается от корневого каталога
+    outDir: 'build',
+    // assetsDir: 'assets', // Установите здесь путь к вашим ресурсам, если он отличается от корневого каталога
   },
 })
