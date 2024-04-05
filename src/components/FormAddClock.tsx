6cd0a302909445de1554a1f9f37566d5382cc472
Mariza0 <maria.net@mail.ru>
2024-04-05 22:12:05 +0200

    change uuid

--- src/components/FormAddClock.tsx
+++ src/components/FormAddClock.tsx
@@ -1,7 +1,8 @@
 // FormAddClock.tsx
 import React, { Component } from 'react';
 import NewClock from './NewClock';
-import { v4 as uuidv4 } from 'uuid';
+// import{ v4 as uuidv4 } from 'uuid';
+const { v4: uuidv4 } = require('uuid');
 
 interface FormAddClockState {
   city: string;
