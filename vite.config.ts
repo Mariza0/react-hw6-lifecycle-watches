6941a6cee5924a8c865f90f126f0f2c5d671aafb
Mariza0 <maria.net@mail.ru>
2024-04-05 21:48:45 +0200

    change badge

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  build: {
+    rollupOptions: {
+      external: ['uuid']
+    }
+  },
   base: "react-hw6-lifecycle-watches",
   assetsInclude: ['src/img'],
   plugins: [react()],
