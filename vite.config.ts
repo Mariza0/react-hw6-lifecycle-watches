c1ca24892f4f9be2a5869289112d35cf18805972
Mariza0 <maria.net@mail.ru>
2024-04-05 17:42:58 +0200

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['uuid']
+    }
+  },
   base: "react-hw6-lifecycle-time",
   assetsInclude: ['src/img'],
   plugins: [react()],
